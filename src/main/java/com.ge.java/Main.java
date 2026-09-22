788ca3b1ac933d415458ed0bde88e7063df2e2bc
Fifalianantsoa <fifaliantsoajeanfrederic@gmail.com>
2026-09-22 18:11:00 +0300

    Add files via upload

--- src/main/java/com.ge.java/Main.java
+++ src/main/java/com.ge.java/Main.java
@@ -1,1 +1,25 @@
+package com.ge.java;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import com.ge.java.database.Database;
+import com.ge.java.ui.FenetreEtudiant;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        // Initialiser la base de données
+        Database.initDatabase();
+
+        FenetreEtudiant fenetre = new FenetreEtudiant();
+
+        fenetre.createWidgets();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
